f27a79ae2b80785514a99ad20dc312568fbe3ef0
ivan_khomichkov <zloivan@mail.ru>
2021-12-01 19:29:27 +0300

    Initial commit

---
+++ Assets/Scripts/ProgressBar/ProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour
+{
+
+    
+    #if UNITY_EDITOR
+    void Update()
+    {
+        //mylogs Probably remove this later
+        if (Debug.isDebugBuild) Debug.Log($"<color=purple>Test</color>");
+
+    }
+    
+    #endif
+    
+    
+    public Image fillAmount;
+    public RectTransform parent;
+    public RectTransform child;
+    
+
+    private void UpdateSize(float value)
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateSize(5f);
+    }
+
+    // Update is called once per frame
+    
+}
